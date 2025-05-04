eaac5c8de398ebc9efab3d7f5e5e678c44a21362
Oscar Bao <oscarbaoxianhao@gmail.com>
2025-05-04 16:39:35 -0400

    Add memory and clk module

---
+++ FPGA_Client/memory.sv
@@ -0,0 +1,41 @@
+module memory #(
+    parameter INIT_FILE = ""
+)(
+    input logic     clk,
+    input logic     [3:0] selector,
+    output logic    [9:0] read_data
+);
+
+    // Declare memory array for storing 512 10-bit samples of a sine function
+    logic [9:0] sample_memory [0:2047];
+    logic [8:0] read_address = 0;
+
+    initial if (INIT_FILE) begin
+        $readmemh(INIT_FILE, sample_memory);
+    end
+
+    always_ff @(posedge clk) begin
+        // Different waveforms
+        case (selector[1:0])
+        2'b00: begin
+            read_data <= sample_memory[read_address];
+        end
+        2'b01: begin
+            read_data <= sample_memory[read_address + 512];
+        end
+        2'b10: begin
+            read_data <= sample_memory[read_address + 1024];
+        end
+        2'b11: begin
+            read_data <= sample_memory[read_address + 1536];
+        end
+        default: read_data <= sample_memory[read_address];
+        endcase
+    end
+
+    always_ff @(posedge clk) begin
+        read_address <= read_address + 1;
+    end
+
+endmodule
+
--- FPGA_Client/top.sv
+++ FPGA_Client/top.sv
@@ -1,7 +1,52 @@
+`include "memory.sv"
+`include "spi_client.sv"
+`include "var_clk.sv"
+
 module top(
     input logic     clk,
     input logic     rst,
-
-    // output logics will be same as MP3 prob
+    input logic     _31b,
+    input logic     _29b,
+    input logic     _37a,
+    output logic    _9b,    // D0
+    output logic    _6a,    // D1
+    output logic    _4a,    // D2
+    output logic    _2a,    // D3
+    output logic    _0a,    // D4
+    output logic    _5a,    // D5
+    output logic    _3b,    // D6
+    output logic    _49a,   // D7
+    output logic    _45a,   // D8
+    output logic    _48b    // D9
 );
+
+    logic [9:0] data;
+    logic [3:0] selector;
+    logic selector_sig;
+    logic clk_out;
+
+    spi_client spi(
+        .clk            (clk),
+        .spi_clk        (_31b),
+        .mosi           (_29b),
+        .cs             (_37a),
+        .command        (selector),
+        .command_signal (selector_sig)
+    )
+
+    var_clk var_clk(
+        .clk            (clk),
+        .selector       (selector),
+        .clk_out        (clk_out)
+    )
+
+    memory #(
+        .INIT_FILE      ("sine.txt")
+    ) u1 (
+        .clk            (clk_out), 
+        .selector       (selector),
+        .read_data      (data)
+    );
+
+    assign {_48b, _45a, _49a, _3b, _5a, _0a, _2a, _4a, _6a, _9b} = data;
 endmodule
